3b97b37f5195ac906725a32e48d797c4b1708486
John Rodriguez <johnmaverickox@gmail.com>
2023-10-21 17:14:58 -0400

    Wild Wild Life answered

--- lab-answers.js
+++ lab-answers.js
@@ -13,15 +13,39 @@
 //Fizz Buzz ///
 
 
-    function fizzBuzz(num){
+    // function fizzBuzz(num){
 
-        for (let i = 1 ; i <= num ; i++){
-            if (i % 3 === 0 && i % 5 === 0) console.log( 'Fizz_Buzz');
-                else if(i % 3 === 0 ) console.log ('Fizz');
-                    else if(i % 5 === 0 ) console.log ('Buzz');
-                        else console.log('.');
+    //     for (let i = 1 ; i <= num ; i++){
+    //         if (i % 3 === 0 && i % 5 === 0) console.log( 'Fizz_Buzz');
+    //             else if(i % 3 === 0 ) console.log ('Fizz');
+    //                 else if(i % 5 === 0 ) console.log ('Buzz');
+    //                     else console.log('.');
     
-        }
+    //     }
 
-    }
-    fizzBuzz(60);
+    // }
+    // fizzBuzz(60);
+
+//Wild Wild Life///
+            //Name    //Animal //Age //Home town
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+//Part 1//
+plantee[2]+= 1;console.log(plantee);
+
+//Part 2//
+wolfy[3]= 'Gotham City';console.log(wolfy);
+
+//Part 3 //
+dart.push('Hawkins');console.log(dart);
+//or
+// dart[4]='Hawkins';console.log(dart);
+
+//Part 4//
+wolfy[0]='Gameboy';console.log(wolfy);
+//or
+wolfy.splice(0,0,'gameboy');console.log(wolfy);
