7eaa978309782c5fdb485c848479050927bbe81f
John Rodriguez <johnmaverickox@gmail.com>
2023-10-21 14:10:04 -0400

    Get Even Answered

--- lab-answers.js
+++ lab-answers.js
@@ -1,4 +1,9 @@
 ///Easy Going////
-for (let i = 0 ; i <= 20 ; i++){
-    console.log(i);
+// for (let i = 0 ; i <= 20 ; i++){
+//     console.log(i);
+// }
+// Get Even///
+for (let i = 0 ; i <= 200 ; i +=2){
+     
+console.log(i);
 }
