563be1b48f1c878d3d03b181d07e0f17ddbfeded
John Rodriguez <johnmaverickox@gmail.com>
2023-10-21 16:15:07 -0400

    Fizz Buzz answered

--- lab-answers.js
+++ lab-answers.js
@@ -2,8 +2,26 @@
 // for (let i = 0 ; i <= 20 ; i++){
 //     console.log(i);
 // }
+
 // Get Even///
-for (let i = 0 ; i <= 200 ; i +=2){
+// for (let i = 0 ; i <= 200 ; i +=2){
      
-console.log(i);
-}
+// console.log(i);
+// }
+
+
+//Fizz Buzz ///
+
+
+    function fizzBuzz(num){
+
+        for (let i = 1 ; i <= num ; i++){
+            if (i % 3 === 0 && i % 5 === 0) console.log( 'Fizz_Buzz');
+                else if(i % 3 === 0 ) console.log ('Fizz');
+                    else if(i % 5 === 0 ) console.log ('Buzz');
+                        else console.log('.');
+    
+        }
+
+    }
+    fizzBuzz(60);
